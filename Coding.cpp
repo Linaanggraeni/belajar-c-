056d513d97b78925296c021ac0d969667f555503
Lina Anggraeni <115996016+Linaanggraeni@users.noreply.github.com>
2022-10-24 19:15:57 +0700

    Create Coding.cpp

---
+++ Coding.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Hello, world!"<<endl; 
+    cout <<"Nama:Lina"<<endl;
+    cout <<"kelas:X pplg 2"<<endl;
+    return 0;
+    }
+
+
