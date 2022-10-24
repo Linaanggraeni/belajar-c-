369fe82499f93de9afa4efa90457859a04dc56a1
Lina Anggraeni <115996016+Linaanggraeni@users.noreply.github.com>
2022-10-24 19:21:04 +0700

    Create Arrayw.cpp

---
+++ Arrayw.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    int nilai [3][5]
+    {
+      {10,20,30,40},
+      {50,60,70,80},
+      {90,100}
+    };
+    cout << "nilai" << nilai[2][1];
+}
+
