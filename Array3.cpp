b45c604efdd3bd5d7bf574716456f36dc276b4e6
Lina Anggraeni <115996016+Linaanggraeni@users.noreply.github.com>
2022-10-24 19:22:10 +0700

    Create Array3.cpp

---
+++ Array3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+int angka [] = {3,5,6,8,12};
+    cout << " angka " << angka [4]<<endl;
+     char huruf[] = {' x ', ' y ', 'z' };
+    printf ( " huruf %c", huruf [1]);
+    string nama[] = { " bagas", " tanaka ", "panca"};
+    cout << " nama = " << nama [0] << endl;
+  }
